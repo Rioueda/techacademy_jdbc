f4d618757ed08090f811ca05ccc6a31fe1307c55
dir-FY24rioueda <rio,ueda.b122@dirbato.co.jp>
2024-04-18 09:29:39 +0900

    0418

--- techacademy_jdbc/src/dbSample/DbConnectSample2.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample2.java
@@ -31,23 +31,20 @@ public class DbConnectSample2 {
             // 4, 5. Select文の実行と結果を格納／代入
             String sql = "SELECT * FROM country LIMIT 50";
             rs = stmt.executeQuery(sql);
-
-            // 6. 結果を表示する
+        
+         // 6. 結果を表示する
             while( rs.next() ){
-                // Name列の値を取得
-                String name = rs.getString("Name");
-                // 取得した値を表示
-                System.out.println(name);
+                 // ループ内は、省略
             }
 
+            // 6-1. データの更新を行う
+            sql = "update country set Population = 105000 where Code = 'ABW'";
+            int count = stmt.executeUpdate(sql);
+            System.out.println(count);
 
-        } catch (ClassNotFoundException e) {
-            System.err.println("JDBCドライバーのロードに失敗しました。");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.err.println("データベースに異常が発生しました。");
-            e.printStackTrace();
-        } finally {
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             // 7. 接続を閉じる
             if( rs != null ){
                 try {
